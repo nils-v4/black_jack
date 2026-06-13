9ac683597225124e63e977d70f6cd0642e6aef11
Nils <verschaeve.nils@gmail.com>
2026-06-13 19:29:50 +0100

    start with some game logic

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,31 @@
 mod card;
 mod deck;
 mod player;
+//use dealer::*;
 use deck::*;
+use player::*;
 use std::io::*;
 
 fn main() {
     let mut deck = Deck::initialize(1);
     deck.shuffle();
-    deck.show_deck();
+    /*deck.show_deck();
     deck.count();
     let card = deck.take_card();
-    card.to_string();
+    card.to_string();*/
 
-    let mut input = String::new();
-    std::io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read input");
+    //let dealer = Dealer::new();
+    let player1 = Player::new(String::from("nilsv"));
+
+    while true {
+        let mut input = String::new();
+        std::io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read input");
+
+        let action: String = match input.trim().parse() {
+            Ok(act) => act,
+            Err(_) => continue,
+        };
+    }
 }
